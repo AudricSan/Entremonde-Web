b5fe55ad497eaf82feaedc8761499948dc923df7
Audric_San <audricrosier@gmail.com>
2022-02-21 19:01:06 +0100

    Update dump.sql
    - insert Role
    - Insert User

--- model/sql/dump.sql
+++ model/sql/dump.sql
@@ -1,2 +1,12 @@
-INSERT INTO role (Role_ID, Role_Name) VALUES (NULL, 'Administrateur'), (NULL, 'Gestionaire'), (NULL, 'Editeur'), (NULL, 'Photographe');
+INSERT INTO role (Role_ID, Role_Name) VALUES
+                 (NULL, 'Administrateur'),
+                 (NULL, 'Gestionaire'),
+                 (NULL, 'Editeur'),
+                 (NULL, 'Photographe');
+
+INSERT INTO user (User_ID, User_Name, User_Firstname, User_login, User_Password, User_Mail, User_Bank, User_Activity) VALUES
+                 (NULL, 'Audric', 'Rosier', 'Audric_San', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1),
+                 (NULL, 'Xavier', 'Deleclos', 'Kikou', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1),
+                 (NULL, 'Ronald', 'Racquez', 'Raquette', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1),
+                 (NULL, 'Benjamin', 'Delbar', 'Laravel', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1);
 
