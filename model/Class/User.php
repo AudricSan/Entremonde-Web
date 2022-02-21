605debd5246d2828003264ab2eba58a146964fb5
Audric_San <audricrosier@gmail.com>
2022-02-21 18:59:20 +0100

    Create User.php

---
+++ model/Class/User.php
@@ -0,0 +1,48 @@
+<?php
+
+class User
+{
+    private string $_login;
+    private string $_name;
+    private string $_firstname;
+
+    private string $_email;
+    private string $_password;
+
+    private string $_bank;
+
+    private int    $_activity;
+    private int    $_id;
+
+    //Constructeur
+    public function __construct($id, $name, $fname, $log, $pass, $mail, $bank, $act)
+    {
+        $this->_login = $log;
+        $this->_name = $name;
+
+        $this->_firstname = $fname;
+        $this->_email = $mail;
+
+        $this->_password = $pass;
+        $this->_bank = $bank;
+
+        $this->_activity = $act;
+        $this->_id = $id;
+    }
+
+    //SUPER SETTER
+    public function __set($prop, $value)
+    {
+        if (property_exists($this, $prop)) {
+            return $this->$prop = $value;
+        }
+    }
+
+    //SUPER GETTER
+    public function __get($prop)
+    {
+        if (property_exists($this, $prop)) {
+            return $this->$prop;
+        }
+    }
+}
