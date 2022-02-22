5d958cc928f3e6bf9cedae613e3dd9054713e0ba
Audric_San <audricrosier@gmail.com>
2022-02-22 16:07:00 +0100

    Add Picture files

---
+++ view/picture/one.php
@@ -0,0 +1,7 @@
+<h2> <?= $picture->_name; ?> </h2>
+
+<h3> <?= $picture->_id;?> </h3>
+<h3> <?= $picture->_description;?> </h3>
+<h3> <?= $picture->_statut;?> </h3>
+<h3> <?= $picture->_tags;?> </h3>
+<h3> <?= $picture->_link;?> </h3>
