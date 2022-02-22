53ec267ea4d02c4e86a7342921935060b4c691e7
Audric_San <audricrosier@gmail.com>
2022-02-22 14:25:02 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,10 +9,17 @@ include ('../model/Class/User.php');
 include ('../model/DAO/UserDAO.php');
 include ('../controller/UserController.php');
 
+include ('../model/Class/Activity.php');
+include ('../model/DAO/ActivityDAO.php');
+include ('../controller/ActivityController.php');
+
 $admin = new AdminController();
 $admin->show(1);
 
 $user = new UserController();
+$user->show(1);
+
+$user = new ActivityController();
 $user->show(1);
 
 
