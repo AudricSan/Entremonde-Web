262ff6bb650e3dce8e7e550ce7fcf6076a82983d
Audric_San <audricrosier@gmail.com>
2022-02-22 14:23:07 +0100

    Update env.php
    - format insode

--- env.php
+++ env.php
@@ -1,11 +1,16 @@
 <?php
 
 $env = [
+    //APP
     'APP_NAME' => 'Entremonde',
-    'APP_KEY' => '937a4a8c13e317dfd28effdd479cad2f',
+    'APP_KEY' => '',
+
+    //DATABASE
     'DB_HOST' => 'localhost',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
     'DB_NAME' => 'entremonde',
     'DB_PORT' => 3306,
+
+    //
 ];
