8df94ab45fa0e3c241bcf32e187a25d2399eedb7
Audric_San <audricrosier@gmail.com>
2022-02-22 16:06:14 +0100

    Create PictureController.php

---
+++ controller/PictureController.php
@@ -0,0 +1,27 @@
+<?php
+class PictureController {
+    private $dao;
+    
+    public function __construct () {
+        $this->dao = new PictureDAO();
+    }
+    
+    public function index () {
+        $pictures = $this->dao->fetchAll();
+        include('../view/picture/list.php');
+    }
+    
+    public function show ($id) {
+        $picture = $this->dao->fetch($id);
+        include('../view/picture/one.php');
+    }
+    
+    public function create () {
+        include('../view/picture/form.php');
+    }
+    
+    public function store($data) {
+        $this->dao->store($data);
+    }
+}
+?>
