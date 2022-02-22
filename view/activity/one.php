50499fd2776151a83155c8e60321428477e5287b
Audric_San <audricrosier@gmail.com>
2022-02-22 14:24:44 +0100

    Create one.php

---
+++ view/activity/one.php
@@ -0,0 +1,8 @@
+<h2> <?= $activity->_name; ?> </h2>
+
+<h3> <?= $activity->_description;?> </h3>
+<h3> <?= $activity->_content;?> </h3>
+<h3> <?= $activity->_date;?> </h3>
+<h3> <?= $activity->_type;?> </h3>
+<h3> <?= $activity->_statut;?> </h3>
+<h3> <?= $activity->_id;?> </h3>
