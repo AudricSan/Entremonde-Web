b442d8c0a87b47014cc5589d53a5e62ab5ac7ad3
Audric_San <audricrosier@gmail.com>
2022-02-22 15:07:31 +0100

    Update db.sql
    - Add col Link in Picture

--- model/sql/db.sql
+++ model/sql/db.sql
@@ -35,6 +35,7 @@ CREATE TABLE `Picture` (
   `Picture_Name` VARCHAR(50) NULL DEFAULT NULL,
   `Picture_Description` VARCHAR(255) NULL DEFAULT NULL,
   `Picture_Statut` INTEGER NULL DEFAULT NULL COMMENT 'Bit => 1 = True (affiché) // 0 = False (Masqué)',
+  `Picture_Link` VARCHAR(50) NULL DEFAULT NULL,
   `Picture_Tags` INTEGER NULL DEFAULT NULL,
   PRIMARY KEY (`Picture_ID`)
 );
