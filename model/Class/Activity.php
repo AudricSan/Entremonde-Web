8e37568ba42375fea210760a16be7e28e6d80fff
Audric_San <audricrosier@gmail.com>
2022-02-22 14:23:12 +0100

    Create Activity.php

---
+++ model/Class/Activity.php
@@ -0,0 +1,41 @@
+<?php
+
+class Activity
+{
+    private string $_name;
+    private string $_description;
+    private string $_content;
+    private string $_date;
+
+    private int    $_type;
+    private int    $_statut;
+    private int    $_id;
+
+    //Constructeur
+    public function __construct($id, $name, $desc, $stat, $cont, $type, $date)
+    {
+        $this->_name = $name;
+        $this->_description = $desc;
+        $this->_content = $cont;
+        $this->_type = intval($type);
+        $this->_date = $date;
+        $this->_statut = intval($stat);
+        $this->_id = intval($id);
+    }
+
+    //SUPER SETTER
+    public function __set($prop, $value)
+    {
+        if (property_exists($this, $prop)) {
+            return $this->$prop = $value;
+        }
+    }
+
+    //SUPER GETTER
+    public function __get($prop)
+    {
+        if (property_exists($this, $prop)) {
+            return $this->$prop;
+        }
+    }
+}
