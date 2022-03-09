6505b1fcf084597daf99bb6a60b5a0b61207c590
Audric_San <audricrosier@gmail.com>
2022-03-09 11:00:22 +0100

    Update db.sql
    - add Role, Type, Statut Table

--- model/sql/db.sql
+++ model/sql/db.sql
@@ -35,8 +35,8 @@ CREATE TABLE `Picture` (
   `Picture_Name` VARCHAR(50) NULL DEFAULT NULL,
   `Picture_Description` VARCHAR(255) NULL DEFAULT NULL,
   `Picture_Statut` INTEGER NULL DEFAULT NULL COMMENT 'Bit => 1 = True (affiché) // 0 = False (Masqué)',
-  `Picture_Link` VARCHAR(50) NULL DEFAULT NULL,
   `Picture_Tags` INTEGER NULL DEFAULT NULL,
+  `Picture_Link` VARCHAR(10) NULL DEFAULT NULL,
   PRIMARY KEY (`Picture_ID`)
 );
 
@@ -179,12 +179,27 @@ CREATE TABLE `Album` (
 );
 
 -- ---
+-- Table 'Statut'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Statut`;
+		
+CREATE TABLE `Statut` (
+  `Statut_ID` INTEGER NOT NULL AUTO_INCREMENT,
+  `Statut_Name` VARCHAR(15) NULL DEFAULT NULL,
+  PRIMARY KEY (`Statut_ID`)
+);
+
+-- ---
 -- Foreign Keys 
 -- ---
 
 ALTER TABLE `Admin` ADD FOREIGN KEY (Admin_Role) REFERENCES `Role` (`Role_ID`);
 ALTER TABLE `Picture` ADD FOREIGN KEY (Picture_Tags) REFERENCES `Tags` (`Tag_ID`);
+ALTER TABLE `Article` ADD FOREIGN KEY (Article_Statut) REFERENCES `Statut` (`Statut_ID`);
 ALTER TABLE `Article` ADD FOREIGN KEY (Article_tags) REFERENCES `Tags` (`Tag_ID`);
+ALTER TABLE `Activity` ADD FOREIGN KEY (Activity_Statut) REFERENCES `Statut` (`Statut_ID`);
 ALTER TABLE `Activity` ADD FOREIGN KEY (Activity_Type) REFERENCES `Type` (`Type_ID`);
 ALTER TABLE `User` ADD FOREIGN KEY (User_Activity) REFERENCES `Activity` (`Activity_ID`);
 ALTER TABLE `OldActivity` ADD FOREIGN KEY (Old_User) REFERENCES `User` (`User_ID`);
@@ -207,6 +222,7 @@ ALTER TABLE `Album` ADD FOREIGN KEY (Album_Atcivity) REFERENCES `Activity` (`Act
 -- ALTER TABLE `Type` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `OldActivity` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `Album` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Statut` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
@@ -214,16 +230,16 @@ ALTER TABLE `Album` ADD FOREIGN KEY (Album_Atcivity) REFERENCES `Activity` (`Act
 
 -- INSERT INTO `Admin` (`Admin_ID`,`Admin_Mail`,`Admin_Login`,`Admin_Password`,`Admin_Name`,`Admin_Firstname`,`Admin_Role`) VALUES
 -- ('','','','','','','');
--- INSERT INTO `Picture` (`Picture_ID`,`Picture_Name`,`Picture_Description`,`Picture_Statut`,`Picture_Tags`) VALUES
--- ('','','','','');
+-- INSERT INTO `Picture` (`Picture_ID`,`Picture_Name`,`Picture_Description`,`Picture_Statut`,`Picture_Tags`,`Picture_Link`) VALUES
+-- ('','','','','','');
 -- INSERT INTO `Article` (`Article_ID`,`Article_Name`,`Article_Description`,`Article_Text`,`Article_Statut`,`Article_tags`) VALUES
 -- ('','','','','','');
 -- INSERT INTO `Activity` (`Activity_ID`,`Activity_Name`,`Activity_Description`,`Activity_Statut`,`Activity_Content`,`Activity_Type`,`Activity_Date`) VALUES
 -- ('','','','','','','');
 -- INSERT INTO `Newletter` (`Newsletter_ID`,`Newsletter_Mail`) VALUES
 -- ('','');
--- INSERT INTO `User` (`User_ID`,`User_Name`,`User_Firstname`,`User_login`,`User_Password`,`User_Mail`,`User_Bank`,`User_Activity`) VALUES
--- ('','','','','','','','');
+-- INSERT INTO `User` (`User_ID`,`User_Name`,`User_Firstname`,`User_login`,`User_Password`,`User_Mail`,`User_Bank`,`User_Activity`,`User_Age`,`User_Birthday`,`User_Point`) VALUES
+-- ('','','','','','','','','','','');
 -- INSERT INTO `Tags` (`Tag_ID`,`Tag_Name`) VALUES
 -- ('','');
 -- INSERT INTO `Role` (`Role_ID`,`Role_Name`) VALUES
@@ -234,3 +250,5 @@ ALTER TABLE `Album` ADD FOREIGN KEY (Album_Atcivity) REFERENCES `Activity` (`Act
 -- ('','','');
 -- INSERT INTO `Album` (`Album_ID`,`Album_Name`,`Album_Picture`,`Album_Atcivity`) VALUES
 -- ('','','','');
+-- INSERT INTO `Statut` (`Statut_ID`,`Statut_Name`) VALUES
+-- ('','');
