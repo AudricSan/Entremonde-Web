fb1114624b838ee455f05063ba5c6e5561c2b4a8
Audric_San <audricrosier@gmail.com>
2022-03-09 11:01:05 +0100

    Update dump.sql
    - Add Statut
    - Add Type

--- model/sql/dump.sql
+++ model/sql/dump.sql
@@ -1,17 +1,17 @@
 INSERT INTO role (Role_ID, Role_Name) VALUES
-                 (NULL, 'Administrateur'),
-                 (NULL, 'Gestionaire'),
-                 (NULL, 'Editeur'),
-                 (NULL, 'Photographe');
+                 (NULL, "Administrateur"),
+                 (NULL, "Gestionaire"),
+                 (NULL, "Editeur"),
+                 (NULL, "Photographe");
 
 INSERT INTO user (User_ID, User_Name, User_Firstname, User_login, User_Password, User_Mail, User_Bank, User_Activity) VALUES
-                 (NULL, 'Audric', 'Rosier', 'Audric_San', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1),
-                 (NULL, 'Xavier', 'Deleclos', 'Kikou', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1),
-                 (NULL, 'Ronald', 'Racquez', 'Raquette', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1),
-                 (NULL, 'Benjamin', 'Delbar', 'Laravel', 'coucou', 'test@gmail.com', 'BE00 0000 0000 0000', 1);
+                 (NULL, "Audric", "Rosier", "Audric_San", "coucou", "test@gmail.com", "BE00 0000 0000 0000", 1),
+                 (NULL, "Xavier", "Deleclos", "Kikou", "coucou", "test@gmail.com", "BE00 0000 0000 0000", 1),
+                 (NULL, "Ronald", "Racquez", "Raquette", "coucou", "test@gmail.com", "BE00 0000 0000 0000", 1),
+                 (NULL, "Benjamin", "Delbar", "Laravel", "coucou", "test@gmail.com", "BE00 0000 0000 0000", 1);
 
 INSERT INTO activity (Activity_ID, Activity_Name, Activity_Description, Activity_Statut, Activity_Content, Activity_Type, Activity_Date) VALUES
-                     (NULL, 'Stage Decouverte', 'lorem Impum', 1, 'Un stage de decouvertte du GN avec plein de gens', 1, '2022-02-01');
+                     (NULL, "Stage Decouverte", "lorem Impum", 1, "Un stage de decouvertte du GN avec plein de gens", 1, "2022-02-01");
 
 ALTER TABLE user ADD User_Age INTEGER NULL DEFAULT NULL;
 ALTER TABLE user ADD User_Birthday DATE NULL DEFAULT NULL;
@@ -20,3 +20,21 @@ ALTER TABLE picture ADD Picture_Link VARCHAR(50) NULL DEFAULT NULL;
 
 INSERT INTO picture (Picture_ID, Picture_Name, Picture_Description, Picture_Statut, Picture_Tags, Picture_Link) VALUES
                     (NULL, "Picture Name", "Picture_Description", 1, 1, "Picture_Link");
+
+INSERT INTO `statut` (`Statut_ID`, `Statut_Name`) VALUES
+                     (NULL, "New"),
+                     (NULL, "On Hold"),
+                     (NULL, "Cancelled"),
+                     (NULL, "Finish");
+
+INSERT INTO `type` (`Type_ID`, `Type_Name`) VALUES
+                   (NULL, "Escrime ludique"),
+                   (NULL, "Jeux de rôles"),
+                   (NULL, "Stages d'aventure"),
+                   (NULL, "Stage d'aventure & d'introduction à l'anglais"),
+                   (NULL, "Stage créatif "),
+                   (NULL, "Anniversaires"),
+                   (NULL, "Team Building "),
+                   (NULL, "Ecoles"),
+                   (NULL, "Mariages"),
+                   (NULL, "Déambulations");
