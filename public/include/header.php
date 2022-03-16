f3ea63a80cfbec70a5bed994fe3b2a0dd6a2cbf9
Audric Rosier <audricrosier@gmail.com>
2022-03-16 14:25:47 +0100

    Update header.php

---
+++ public/include/header.php
@@ -0,0 +1,19 @@
+<header id="header">
+            <nav>
+                <!-- Logo-->
+                <figure id="logo"><a href="#"><img src="images/logo.jpg" alt="Logo" title="Company name" width="200"></a></figure>
+                
+                <!--liste-->
+                <ul class="navigation">
+                    <li><a href="#">Onglet 1</a></li>
+                    <li><a href="#contenu">Onglet 2</a></li>
+                    <li><a href="#produits">Onglet 3</a></li>
+                    <li><a href="#galerie">Onglet 4</a></li>
+                    <li><a href="#texte-images">Onglet 5</a></li>
+                    <li><a href="#reseaux"> CONTACT</a></li>
+                </ul>
+
+                <a class="loggin">loggin</a>
+                <a class="inscription">Je m'inscris</a>
+            </nav>
+</header>
