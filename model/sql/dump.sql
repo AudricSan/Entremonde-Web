482d05fc39a94e65648fea0ed2d91eadcf65ef06
Audric Rosier <audricrosier@gmail.com>
2022-03-16 16:45:42 +0100

    Update dump.sql
    - add activity media
    - add activity price
    
    - Add some activity

--- model/sql/dump.sql
+++ model/sql/dump.sql
@@ -21,13 +21,13 @@ ALTER TABLE picture ADD Picture_Link VARCHAR(50) NULL DEFAULT NULL;
 INSERT INTO picture (Picture_ID, Picture_Name, Picture_Description, Picture_Statut, Picture_Tags, Picture_Link) VALUES
                     (NULL, "Picture Name", "Picture_Description", 1, 1, "Picture_Link");
 
-INSERT INTO `statut` (`Statut_ID`, `Statut_Name`) VALUES
+INSERT INTO statut (Statut_ID, Statut_Name) VALUES
                      (NULL, "New"),
                      (NULL, "On Hold"),
                      (NULL, "Cancelled"),
                      (NULL, "Finish");
 
-INSERT INTO `type` (`Type_ID`, `Type_Name`) VALUES
+INSERT INTO type (Type_ID, Type_Name) VALUES
                    (NULL, "Escrime ludique"),
                    (NULL, "Jeux de rôles"),
                    (NULL, "Stages d'aventure"),
@@ -39,14 +39,24 @@ INSERT INTO `type` (`Type_ID`, `Type_Name`) VALUES
                    (NULL, "Mariages"),
                    (NULL, "Déambulations");
 
-INSERT INTO `statut` (`Statut_ID`, `Statut_Name`) VALUES
+INSERT INTO statut (Statut_ID, Statut_Name) VALUES
                      (NULL, "Public"),
                      (NULL, "Private");
                      
-INSERT INTO `tags` (`Tag_ID`, `Tag_Name`) VALUES
+INSERT INTO tags (Tag_ID, Tag_Name) VALUES
                      (NULL, "Craft"),
                      (NULL, "Portrait"),
                      (NULL, "Extérieur"),
                      (NULL, "Poses"),
                      (NULL, "Live"),
                      (NULL, "Groupe");
+
+INSERT INTO activity (Activity_ID, Activity_Name, Activity_Description, Activity_Statut, Activity_Content, Activity_Type, Activity_Date) VALUES 
+                     (NULL,       'Activity 1',   'Desciption',         1,              'Activity_Content', 1,            '2022-03-02'),
+                     (NULL,       'Activity 2',   'Desciption',         1,              'Activity_Content', 2,            '2022-03-02'),
+                     (NULL,       'Activity 3',   'Desciption',         1,              'Activity_Content', 4,            '2022-03-02'),
+                     (NULL,       'Activity 4',   'Desciption',         1,              'Activity_Content', 6,           '2022-03-02');
+
+ALTER TABLE activity ADD Activity_Price INT NULL AFTER Activity_Date;
+
+ALTER TABLE activity ADD Activity_Media VARCHAR(11) NOT NULL AFTER Activity_Price;
