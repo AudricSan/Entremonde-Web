2c01fab735ac26212b9473a51b1d0169030d02f3
Audric Rosier <audricrosier@gmail.com>
2022-03-16 14:25:45 +0100

    Update footer.php

---
+++ public/include/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <ul id="reseaux">
+        <li><a><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+    </ul>
+
+    <div>
+        <p>Mentions légales, ect. | &copy; Xavier Deleclos
+            <br /><br /> IFOSUP - Le 16 Juin 2021
+        </p>
+    </div>
+
+    <p>
+        <a href=#header class="fas fa-arrow-up arrowUp"></a> <i></i>
+    </p>
+
+</footer>
