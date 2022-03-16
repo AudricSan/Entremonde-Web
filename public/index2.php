53825073100dbfb6bc346f919cfaabeb81da48cc
Weisward <deleclos.xavier@hotmail.com>
2022-03-16 16:47:36 +0100

    Create index2.php

---
+++ public/index2.php
@@ -0,0 +1,46 @@
+<?php
+include ('../model/autoload.php');
+
+include ('../model/Class/Admin.php');
+include ('../model/DAO/AdminDAO.php');
+include ('../controller/AdminController.php');
+
+include ('../model/Class/User.php');
+include ('../model/DAO/UserDAO.php');
+include ('../controller/UserController.php');
+
+include ('../model/Class/Activity.php');
+include ('../model/DAO/ActivityDAO.php');
+include ('../controller/ActivityController.php');
+
+include ('../model/Class/Picture.php');
+include ('../model/DAO/PictureDAO.php');
+include ('../controller/PictureController.php');
+
+include ('../model/Class/Role.php');
+require ('../model/DAO/RoleDAO.php');
+require ('../controller/RoleController.php');
+
+
+$admin = new AdminController();
+    // $admin->show(1);
+    $admin->index();
+
+$user = new UserController();
+    // $user->show(1);
+    $user->index();
+
+$activity = new ActivityController();
+    // $activity->show(1);
+    $activity->index();
+
+$picture = new PictureController();
+    // $picture->show(1);
+    $picture->index();
+
+    $admin->create();
+    $user->create();
+    $activity->create();
+    $picture->create();
+
+?>
