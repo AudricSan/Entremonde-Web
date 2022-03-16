3c8065ac23f13975ca313da5933c3d56b5ea7493
Audric Rosier <audricrosier@gmail.com>
2022-03-16 17:00:23 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,58 +1,84 @@
-<?php ?>
+<?php
+include('../model/autoload.php');
 
-<html>
+include('../model/Class/Admin.php');
+include('../model/DAO/AdminDAO.php');
+include('../controller/AdminController.php');
+
+include('../model/Class/User.php');
+include('../model/DAO/UserDAO.php');
+include('../controller/UserController.php');
+
+include('../model/Class/Activity.php');
+include('../model/DAO/ActivityDAO.php');
+include('../controller/ActivityController.php');
+
+include('../model/Class/Picture.php');
+include('../model/DAO/PictureDAO.php');
+include('../controller/PictureController.php');
+
+include('../model/Class/Role.php');
+require('../model/DAO/RoleDAO.php');
+require('../controller/RoleController.php');
+
+include('../model/Class/Statut.php');
+require('../model/DAO/StatutDAO.php');
+require('../controller/StatutController.php');
+
+include('../model/Class/Type.php');
+require('../model/DAO/TypeDAO.php');
+require('../controller/TypeController.php');
+
+include('../model/Class/Tag.php');
+require('../model/DAO/TagDAO.php');
+require('../controller/TagController.php');
+
+include("include/header.php");
+?>
+
+<!DOCTYPE HTML>
+<html lang="fr">
+
+<head>
     <meta charset="utf-8">
-    <title>Entremonde ASBL</title>
-    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title>Entremonde</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="keywords" content="référencement,SEO,balise meta keywords">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="auteur" content="Xavier Deleclos">
+
+    <!-- <link rel="stylesheet" type="text/css" href="css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/print.css" media="print" /> -->
+
     <!--icones importées-->
-  
     <script src="https://kit.fontawesome.com/3d76d9e733.js" crossorigin="anonymous"></script>
-    <!-------------------------------------------Navigation---------------------------------------------------------->
+    <!--<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">-->
+</head>
+
+
+
+<body>
+    <div class="open"></div>
     <div class="container">
-      <header id="header">
-        <nav>
-          <!-- Logo-->
-          <figure id="logo"><a href="#"><img src="images/logo.jpg" alt="Logo" title="Company name" width="50"></a></figure>
-          <!--liste-->
-          <ul class="navigation">
-            <li><a href="activites.php">Activités</a></li>
-            <li><a href="medias.php">Médias</a></li>
-            <li><a href="about.php">Qui sommes-nous?</a></li>
-            <li><a href="contacts.php">Contacts</a></li>
-          </ul>
-        </nav>
-      </header>
-    </div>
-  
-    <!-----------------------------------------Warning & Welcome------------------------------------------------------------->
-    <div class="warning">
-      <p>ATTENTION : SITE EN COURS DE CONSTRUCTION</p>
-    </div><br><br>
-  
-    <div class="welcome">
-      <h1>Index</h1>
-      <h2>Bienvenue sur Entremonde ASBL</h2>
-      <p>Entremonde ASBL : Site en construction <br><br> Bonne visite.</p>
-    </div>
-  
-    <br><br>
-  
-    <!--<footer class="foot">
-      <div id="reseaux">
-        <a><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-linkedin-in"></i></a>
-      </div>
-  
-      <div>
-        <p>Mentions légales, ect. | &copy; Test
-          <br /><br /> Test2
-        </p>
-      </div>
-  
-      <p>
-        <a href=#header class="fas fa-arrow-up arrowUp"></a> <i></i>
-      </p>
-    </footer>-->
-  
-  </html>
+
+        <!-- Intro -->
+        <section id="accroche">
+            <p>stages, anniv</p>
+            <h1>Osez la grande aventure</h1>
+            <div class="button" href="#"><span>C'est par ici</span></div>
+        </section>
+
+        <!--Section stages-->
+        <section id="sectionstage">
+            <?php gethome(); ?>
+        </section>
+</body>
+
+<?php include("include/footer.php"); ?>
+
+<script src="video.js"></script>
+</html>
