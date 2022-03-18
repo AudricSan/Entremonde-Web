509b9a14a0e8c8e87001c3f0277515ed0dff2a47
Audric Rosier <audricrosier@gmail.com>
2022-03-18 11:48:42 +0100

    Update index.php
    - move include

--- public/index.php
+++ public/index.php
@@ -1,39 +1,6 @@
 <?php
-include('../model/autoload.php');
-
-include('../model/Class/Admin.php');
-include('../model/DAO/AdminDAO.php');
-include('../controller/AdminController.php');
-
-include('../model/Class/User.php');
-include('../model/DAO/UserDAO.php');
-include('../controller/UserController.php');
-
-include('../model/Class/Activity.php');
-include('../model/DAO/ActivityDAO.php');
-include('../controller/ActivityController.php');
-
-include('../model/Class/Picture.php');
-include('../model/DAO/PictureDAO.php');
-include('../controller/PictureController.php');
-
-include('../model/Class/Role.php');
-require('../model/DAO/RoleDAO.php');
-require('../controller/RoleController.php');
-
-include('../model/Class/Statut.php');
-require('../model/DAO/StatutDAO.php');
-require('../controller/StatutController.php');
-
-include('../model/Class/Type.php');
-require('../model/DAO/TypeDAO.php');
-require('../controller/TypeController.php');
-
-include('../model/Class/Tag.php');
-require('../model/DAO/TagDAO.php');
-require('../controller/TagController.php');
-
-include("include/header.php"); ?>
+    include_once("include/header.php");
+?>
 
 <!-- Intro -->
 <section id="accroche">
