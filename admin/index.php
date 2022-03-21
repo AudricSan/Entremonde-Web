73435fdc719ccd98e617ae0067bd6b0303e7d9fd
Audric Rosier <audricrosier@gmail.com>
2022-03-21 16:20:13 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,18 @@
-<?php 
-if (!isset($_SESSION['Admin'])) {
-    header('location: log.php');
+<?php
+session_start();
+// var_dump($_SESSION);
+
+include('../model/autoload.php');
+
+if (empty($_SESSION)) {
+    header('location: login.php');
 
 } else {
-    if ($_SESSION['Admin'] !== true) {
-        unset($_SERVER['Admin']);    
+    if (existadmin($_SESSION['admin'])) {
+        echo ('COUCOU admin : ' . $_SESSION['admin']);
+
+    } else {
+        unset($_SERVER['admin']);
         header('location: /');
     }
 }
-
