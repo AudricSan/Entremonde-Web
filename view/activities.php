26e42104401199e80511cae2b43ac8e16d556975
Audric Rosier <audricrosier@gmail.com>
2022-03-23 17:33:13 +0100

    Create Basic pages

---
+++ view/activities.php
@@ -0,0 +1,23 @@
+<?php
+$title = 'about us';
+include_once("../public/include/header.php");
+?>
+
+<section id="texte-images">
+    <article>
+        <h2>Activités et stages</h2><br>
+        <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+        </p>
+    </article>
+</section>
+
+<section class="activite">
+    <?php getactivities(); ?>
+</section>
+
+<?php include_once("../public/include/footer.php"); ?>
